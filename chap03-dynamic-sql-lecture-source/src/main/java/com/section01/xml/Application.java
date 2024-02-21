af401f18961262303bc6644543ea7b96ed3db032
daersh <daersh@naver.com>
2024-02-21 11:45:31 +0900

    2. choose

--- chap03-dynamic-sql-lecture-source/src/main/java/com/section01/xml/Application.java
+++ chap03-dynamic-sql-lecture-source/src/main/java/com/section01/xml/Application.java
@@ -20,7 +20,7 @@ public class Application {
                     ifSubMenu();
                     break;
                 case 2:
-
+                    chooseSubMenu();
                     break;
                 case 3:
 
@@ -79,4 +79,33 @@ public class Application {
     }
 
 
+
+    public static void chooseSubMenu() {
+        Scanner sc = new Scanner(System.in);
+        MenuService menuService = new MenuService();
+        do {
+            System.out.println("====== choose 서브 메뉴 ======");
+            System.out.println("1. 카테고리 상위 분류별 메뉴 보여주기(식사, 음력, 디저트)");
+            System.out.println("9. 이전 메뉴로");
+            System.out.print("메뉴 번호 입력: ");
+            int no = sc.nextInt();
+
+            switch (no){
+                case 1:
+                    menuService.searchMenuBySupCategory(inputSupCategory());
+                    break;
+                case 9:
+                    return;
+            }
+
+        }while (true);
+    }
+
+    private static SearchCriteria inputSupCategory() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("상위 분류 입력(식사, 음료, 디저트): ");
+        String value= sc.nextLine();
+
+        return new SearchCriteria("category",value);
+    }
 }
--- chap03-dynamic-sql-lecture-source/src/main/java/com/section01/xml/MenuService.java
+++ chap03-dynamic-sql-lecture-source/src/main/java/com/section01/xml/MenuService.java
@@ -24,4 +24,15 @@ public class MenuService {
         System.out.println("service: ");
         menus.forEach(System.out::println);
     }
+
+    public void searchMenuBySupCategory(SearchCriteria searchCriteria) {
+        SqlSession sqlSession = Template.GetSqlSession();
+        MenuMapper mapper = sqlSession.getMapper(MenuMapper.class);
+        List<MenuDTO> menus = mapper.searchMenuBySupCategory(searchCriteria);
+        if(menus!=null && menus.size()>0)
+            menus.forEach(System.out::println);
+        else
+            System.out.println("db 연동 실패 혹은 검색 결과 없음");
+
+    }
 }
